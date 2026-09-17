77bae2ab2884e2cf75076d1ed7d817282f7eaedf
garamilo <giramilo@gmail.com>
2026-09-17 10:37:56 +0800

    update setup_cu128
    update patterned from setup_cu130

--- setup_cu128.sh
+++ setup_cu128.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 set -e
 
-conda create -n tqdne_stead python=3.11 -y
+conda create -n tqdne python=3.11 -y
 
-conda install -n tqdne_stead main::setuptools==80.10.2 -y
-conda install -n tqdne_stead main::numpy==1.25.2 -y
-conda install -n tqdne_stead main::einops==0.8.1 -y
-conda install -n tqdne_stead main::h5py==3.13.0 -y
-conda install -n tqdne_stead main::librosa==0.11.0 -y
-conda install -n tqdne_stead main::jupyterlab==4.2.5 -y
-conda install -n tqdne_stead main::scikit-image==0.25.2 -y
-conda install -n tqdne_stead main::scikit-learn==1.6.1 -y
-conda install -n tqdne_stead main::seaborn==0.13.2 -y
-conda install -n tqdne_stead main::tqdm==4.68.2 -y
+conda install -n tqdne main::setuptools==80.10.2 -y
+conda install -n tqdne main::numpy==1.25.2 -y
+conda install -n tqdne main::einops==0.8.1 -y
+conda install -n tqdne main::h5py==3.13.0 -y
+conda install -n tqdne main::librosa==0.11.0 -y
+conda install -n tqdne main::jupyterlab==4.2.5 -y
+conda install -n tqdne main::scikit-image==0.25.2 -y
+conda install -n tqdne main::scikit-learn==1.6.1 -y
+conda install -n tqdne main::seaborn==0.13.2 -y
+conda install -n tqdne main::tqdm==4.68.2 -y
 
-echo "=== [pip 1/8] installing pytorch-lightning==2.5.1 ==="
-conda run -n tqdne_stead pip install pytorch-lightning==2.5.1
+echo "=== [pip 1-5/10] installing pytorch lightning, obspy, seisbench, torchmetrics, and wandb ==="
+conda run -n tqdne pip install pytorch-lightning==2.5.1 obspy==1.4.1 seisbench==0.8.2 torchmetrics==1.7.0 wandb==0.25.0
 
-echo "=== [pip 2/8] installing obspy==1.4.1 ==="
-conda run -n tqdne_stead pip install obspy==1.4.1
+echo "=== [pip 6-8/10] installing torch, torchvision, torchaudio (cu128) ==="
+conda run -n tqdne pip install torch==2.11.0 torchvision==0.26.0 torchaudio==2.11.0 --index-url https://download.pytorch.org/whl/cu128
 
-echo "=== [pip 3/8] installing seisbench==0.8.2 ==="
-conda run -n tqdne_stead pip install seisbench==0.8.2
+echo "=== [pip 9/10] installing oq-engine requirements ==="
+conda run -n tqdne pip install -r $PWD/experiments_stead/envs/oq-engine_requirements-py311-linux64.txt
 
-echo "=== [pip 4/8] installing torch, torchvision, torchaudio (cu128) ==="
-conda run -n tqdne_stead pip install torch==2.11.0 torchvision==0.26.0 torchaudio==2.11.0 --index-url https://download.pytorch.org/whl/cu128
-
-echo "=== [pip 5/8] installing torchmetrics==1.7.0 ==="
-conda run -n tqdne_stead pip install torchmetrics==1.7.0
-
-echo "=== [pip 6/8] installing wandb==0.25.0 ==="
-conda run -n tqdne_stead pip install wandb==0.25.0
-
-echo "=== [pip 7/8] installing oq-engine requirements ==="
-conda run -n tqdne_stead pip install -r $PWD/experiments_stead/envs/oq-engine_requirements-py311-linux64.txt
-
-echo "=== [pip 8/8] installing tqdne local package (editable install) ==="
-conda run -n tqdne_stead pip install -e $PWD
+echo "=== [pip 10/10] installing tqdne local package (editable install) ==="
+conda run -n tqdne pip install -e $PWD
 
 echo ""
-echo ">>> All installations complete. Environment 'tqdne_stead' is ready. <<<"
+echo ">>> All installations complete. Environment 'tqdne' is ready. <<<"
